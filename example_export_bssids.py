a14932e40465abd6f4c8249e9e8993fe0a107da6
david whitlow <david.whitlow@meraki.com>
2017-10-18 15:58:45 -0700

    add sandbox api key

--- example_export_bssids.py
+++ example_export_bssids.py
@@ -1,7 +1,7 @@
 
 from meraki_bssid_getter import MerakiBssidGetter
 
-api_key = 'e2aca9806dff8a983e8e21590e2e95416986ee9b'
+api_key = 'cc54e1f9520616813f654aab8e0dfc614e33c179 '
 
 m = MerakiBssidGetter(api_key)
 m.export_org_bssids_to_csv('549236','sandbox-bssids.csv')
